ae8c189f79e527c49fd9eb0c7756b549cafd2c04
amora7741 <mora7741@gmail.com>
2024-03-29 12:37:23 -0700

    Add go home link to error page

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,9 +1,11 @@
+import { Link } from 'react-router-dom';
+
 const ErrorPage = () => {
   return (
-    <>
-      <h1>Error</h1>
-      <h2>Sry :(</h2>
-    </>
+    <main>
+      <h1>Error! :(</h1>
+      <Link to='/'>Go Home! </Link>
+    </main>
   );
 };
 
