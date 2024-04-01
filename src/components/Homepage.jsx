0817d530d3a4674d50ae7abb7cec7efbd4edc544
amora7741 <mora7741@gmail.com>
2024-04-01 15:36:41 -0700

    Startup api on homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,7 +1,21 @@
+import { useEffect } from 'react';
 import Waldo from '../assets/Waldohome.png';
 import { Link } from 'react-router-dom';
 
 const HomePage = () => {
+  const fetchHome = async () => {
+    try {
+      const URI = `${import.meta.env.VITE_BASE_API}/`;
+      await fetch(URI);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchHome();
+  }, []);
+
   return (
     <main>
       <img id='waldohome' src={Waldo} alt='' />
